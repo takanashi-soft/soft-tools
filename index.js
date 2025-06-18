dcd75aebd1da6b53ee5b735a56361315a60804f6
Takanashi Hoshino (Official) <glytmxdev@gmail.com>
2025-06-18 12:31:47 -0300

    `[ Takanashi - Tools ]`

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const { chatgpt, mylogic, takanashi, exploit, github, vault, yotools, fbdl, igdl, ttdl, ytmp3, ytmp4 } = require("./soft");
+const { ⱂⱁⰣⰅ, ⱄⰶⰊⰆ, ⰲⰃⰙⰅ, ⰲⰱⱊⰣ, ⰈⰋⰙⰆ, ⰊⰥⱎⰄ, ⱊⰷⱃⰄ, ⰑⰪⰞⰆ, ⰲⰬⱎⰅ, ⰆⰗⰲⰅ, ⰧⱀⰫⰄ, ⱆⱏⱀⰄ } = require("./soft");
+
+const softgpt = ⱂⱁⰣⰅ;
+const softlogic = ⱄⰶⰊⰆ;
+const softtk = ⰲⰃⰙⰅ;
+const softxex = ⰲⰱⱊⰣ;
+const softgit = ⰈⰋⰙⰆ;
+const softvlt = ⰊⰥⱎⰄ;
+const softytl = ⱊⰷⱃⰄ;
+const softfb = ⰑⰪⰞⰆ;
+const softig = ⰲⰬⱎⰅ;
+const softtt = ⰆⰗⰲⰅ;
+const softmp3 = ⰧⱀⰫⰄ;
+const softmp4 = ⱆⱏⱀⰄ;
 
 module.exports = {
   ai: {
-    chatgpt,
-    mylogic,
-    takanashi,
-    exploit
+   chatgpt: softgpt,
+   mylogic: softlogic,
+   takanashi: softtk,
+   exploit: softxex
   },
   uploader: {
-    github,
-    vault,
-    yotools
+   github: softgit,
+   vault: softvlt,
+   yotools: softytl
   },
   downloader: {
-    fbdl,
-    igdl,
-    ttdl,
-    ytmp3,
-    ytmp4
+   facebook: softfb,
+   instagram: softig,
+   tiktok: softtt,
+   ytmp3: softmp3,
+   ytmp4: softmp4
   }
 };
